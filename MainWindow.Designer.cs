7c9fe7162d07cd2fe452abbc8906bb54d250cada
lvl7 <lvl7gg@gmail.com>
2016-04-23 18:08:44 +0200

    Desig of application

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace GetData
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.flowLayoutPanelNavButtons = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.tableLayoutPanelData = new System.Windows.Forms.TableLayoutPanel();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelPhoneNumber = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.flowLayoutPanelNavButtons.SuspendLayout();
+            this.tableLayoutPanelData.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanelMain.Controls.Add(this.flowLayoutPanelNavButtons, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelData, 0, 0);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 2;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(354, 191);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonBack.Location = new System.Drawing.Point(189, 3);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 0;
+            this.buttonBack.Text = "< Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanelNavButtons
+            // 
+            this.flowLayoutPanelNavButtons.Controls.Add(this.buttonNext);
+            this.flowLayoutPanelNavButtons.Controls.Add(this.buttonBack);
+            this.flowLayoutPanelNavButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelNavButtons.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanelNavButtons.Location = new System.Drawing.Point(3, 159);
+            this.flowLayoutPanelNavButtons.Name = "flowLayoutPanelNavButtons";
+            this.flowLayoutPanelNavButtons.Size = new System.Drawing.Size(348, 29);
+            this.flowLayoutPanelNavButtons.TabIndex = 0;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonNext.Location = new System.Drawing.Point(270, 3);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "Next >";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // tableLayoutPanelData
+            // 
+            this.tableLayoutPanelData.ColumnCount = 2;
+            this.tableLayoutPanelData.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelData.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanelData.Controls.Add(this.textBoxPhoneNumber, 1, 3);
+            this.tableLayoutPanelData.Controls.Add(this.textBoxAddress, 1, 2);
+            this.tableLayoutPanelData.Controls.Add(this.textBoxLastName, 1, 1);
+            this.tableLayoutPanelData.Controls.Add(this.labelFirstName, 0, 0);
+            this.tableLayoutPanelData.Controls.Add(this.labelLastName, 0, 1);
+            this.tableLayoutPanelData.Controls.Add(this.labelAddress, 0, 2);
+            this.tableLayoutPanelData.Controls.Add(this.labelPhoneNumber, 0, 3);
+            this.tableLayoutPanelData.Controls.Add(this.textBoxFirstName, 1, 0);
+            this.tableLayoutPanelData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelData.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelData.Name = "tableLayoutPanelData";
+            this.tableLayoutPanelData.RowCount = 4;
+            this.tableLayoutPanelData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelData.Size = new System.Drawing.Size(348, 150);
+            this.tableLayoutPanelData.TabIndex = 1;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(3, 12);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(81, 13);
+            this.labelFirstName.TabIndex = 0;
+            this.labelFirstName.Text = "First name:";
+            this.labelFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labelFirstName.Click += new System.EventHandler(this.labelName_Click);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(3, 49);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(81, 13);
+            this.labelLastName.TabIndex = 1;
+            this.labelLastName.Text = "Last name:";
+            this.labelLastName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(3, 86);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(81, 13);
+            this.labelAddress.TabIndex = 2;
+            this.labelAddress.Text = "Address:";
+            this.labelAddress.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labelAddress.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelPhoneNumber
+            // 
+            this.labelPhoneNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelPhoneNumber.AutoSize = true;
+            this.labelPhoneNumber.Location = new System.Drawing.Point(3, 124);
+            this.labelPhoneNumber.Name = "labelPhoneNumber";
+            this.labelPhoneNumber.Size = new System.Drawing.Size(81, 13);
+            this.labelPhoneNumber.TabIndex = 3;
+            this.labelPhoneNumber.Text = "Phone number:";
+            this.labelPhoneNumber.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labelPhoneNumber.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFirstName.Location = new System.Drawing.Point(90, 8);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(255, 20);
+            this.textBoxFirstName.TabIndex = 4;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLastName.Location = new System.Drawing.Point(90, 45);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(255, 20);
+            this.textBoxLastName.TabIndex = 5;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAddress.Location = new System.Drawing.Point(90, 82);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(255, 20);
+            this.textBoxAddress.TabIndex = 6;
+            // 
+            // textBoxPhoneNumber
+            // 
+            this.textBoxPhoneNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(90, 120);
+            this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(255, 20);
+            this.textBoxPhoneNumber.TabIndex = 7;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 191);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.MaximumSize = new System.Drawing.Size(450, 250);
+            this.MinimumSize = new System.Drawing.Size(370, 200);
+            this.Name = "FormMain";
+            this.Text = "GetData";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.flowLayoutPanelNavButtons.ResumeLayout(false);
+            this.tableLayoutPanelData.ResumeLayout(false);
+            this.tableLayoutPanelData.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelNavButtons;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelData;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelPhoneNumber;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxPhoneNumber;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxLastName;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetData
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
